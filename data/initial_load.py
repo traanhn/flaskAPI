10343c3fd267c916e41708cf485f3b82fa682d0a
Tra Anh <traanh92@gmail.com>
2022-03-13 22:49:07 +0200

    Refined the file path

--- data/initial_load.py
+++ data/initial_load.py
@@ -1,11 +1,12 @@
 import sqlite3
 import json
+import os
 
 
 def initial_load():
     insert_user_query = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)"
     truncate_user_query = "DELETE FROM users"
-    with open('data/data.json') as fp:
+    with open(os.path.join('data','data.json')) as fp:
         data = json.load(fp)
         with sqlite3.connect("data.db") as conn:
             conn.execute(truncate_user_query)
